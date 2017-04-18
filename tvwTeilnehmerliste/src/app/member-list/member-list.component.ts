b0b8e32108a8b8c2ffdd8e928ee571b7d828b521
DESKTOP-3LLF12T\r4smus <christian.kienle85@googlemail.com>
2017-04-18 14:13:49 +0200

    Create a static list of Trainings and display it #5

--- tvwTeilnehmerliste/src/app/member-list/member-list.component.ts
+++ tvwTeilnehmerliste/src/app/member-list/member-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Training } from '../training';
 
 @Component({
   selector: 'app-member-list',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MemberListComponent implements OnInit {
 
+  trainings: Training[]= [
+                          {id: 1,
+                          date: '20.03.2017',
+                          trainers: [
+                                      { id: 1, firstName: 'Stefan', lastName: 'Böck', telNumber: '01254785565'}
+                                    ],
+                          members: [
+                                      {  id: 1, firstName: 'Christian', lastName: 'Kienle', telNumber: '08236473'},
+                                      {  id: 2, firstName: 'Andreas', lastName: 'Haugg', telNumber: '0823652547'},
+                                      {  id: 3, firstName: 'Fabian', lastName: 'Winzig', telNumber: '0823245645'}
+                                   ]},
+                          {id: 2,
+                          date: '11.04.2017',
+                          trainers: [
+                                      { id: 2, firstName: 'Martin', lastName: 'Bröll', telNumber: '01254785565'}
+                                    ],
+                          members: [
+                                      {  id: 1, firstName: 'Christian', lastName: 'Kienle', telNumber: '08236473'},
+                                      {  id: 2, firstName: 'Andreas', lastName: 'Haugg', telNumber: '0823652547'},
+                                      {  id: 3, firstName: 'Fabian', lastName: 'Winzig', telNumber: '0823245645'},
+                                      {  id: 4, firstName: 'Max', lastName: 'Groß', telNumber: '0823245645'},
+                                      {  id: 5, firstName: 'Hans', lastName: 'Wurst', telNumber: '02016546566'}
+                                   ]}
+                         ];
+
+
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
---
+++ tvwTeilnehmerliste/src/app/training.ts
@@ -0,0 +1,8 @@
+import { Member } from './member';
+
+export class Training {
+    id: number;
+    date: string;
+    trainers: Member[];
+    members: Member[];
+}
