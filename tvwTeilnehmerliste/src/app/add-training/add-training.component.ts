dfd58dbcbf5600bb047874bd9647de567b130342
DESKTOP-3LLF12T\r4smus <christian.kienle85@googlemail.com>
2017-04-23 18:24:39 +0200

    Create the add new Training form #8

--- tvwTeilnehmerliste/src/app/add-training/add-training.component.ts
+++ tvwTeilnehmerliste/src/app/add-training/add-training.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from '../member';
 
 @Component({
   selector: 'app-add-training',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-training.component.css']
 })
 export class AddTrainingComponent implements OnInit {
+  trainers: Member[] = [
+                         { id: 1, firstName: 'Stefan', lastName: 'Böck', telNumber: '01254785565'},
+                         { id: 2, firstName: 'Martin', lastName: 'Bröll', telNumber: '01254785565'},
+                         { id: 3, firstName: 'Tom', lastName: 'Körtge', telNumber: '01254785565'}
+                       ];
+  members: Member[] =  [
+                         {  id: 1, firstName: 'Christian', lastName: 'Kienle', telNumber: '08236473'},
+                         {  id: 2, firstName: 'Andreas', lastName: 'Haugg', telNumber: '0823652547'},
+                         {  id: 3, firstName: 'Fabian', lastName: 'Winzig', telNumber: '0823245645'},
+                         {  id: 4, firstName: 'Max', lastName: 'Groß', telNumber: '0823245645'},
+                         {  id: 5, firstName: 'Hans', lastName: 'Wurst', telNumber: '02016546566'}
+                       ];
 
   constructor() { }
 
