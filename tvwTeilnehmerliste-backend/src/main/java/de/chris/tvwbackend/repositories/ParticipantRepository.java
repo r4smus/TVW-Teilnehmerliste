24f9655d2cd0ad51284ff4af906758b3fa68fa91
DESKTOP-3LLF12T\r4smus <christian.kienle85@googlemail.com>
2017-12-29 11:40:34 +0100

    Change add-training component to get the data from the backend #16

---
+++ tvwTeilnehmerliste-backend/src/main/java/de/chris/tvwbackend/repositories/ParticipantRepository.java
@@ -0,0 +1,14 @@
+package de.chris.tvwbackend.repositories;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.repository.CrudRepository;
+
+import de.chris.tvwbackend.models.Member;
+import de.chris.tvwbackend.models.Participant;
+
+
+@Transactional
+public interface ParticipantRepository extends CrudRepository<Participant, Integer>  {
+
+}
